98c7ef949ef3af2b6161bf32492f7f3a8121ba02
vedant meshram <119072082+vedant890@users.noreply.github.com>
2024-02-06 17:40:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showsidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.right = '0';
+}
+
+function hidesidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.right = '-250px';
+}
+
